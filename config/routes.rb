aebc4067a0ba2d1db05648f3ade8d84a6e11e4f3
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-06-05 07:47:00 +0000

    [Update] 編集ホームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
   get 'lists' => 'lists#index'
-  get 'lists/edit'
   get '/top' => 'home#top'
   post 'lists' => 'lists#create'
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
